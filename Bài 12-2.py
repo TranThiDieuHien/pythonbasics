28cba9718d6e3a5152166eaa613f1d87ca5e75f5
TranThiDieuHien <72079838+TranThiDieuHien@users.noreply.github.com>
2020-11-27 22:26:35 +0700

    Add files via upload

--- Bài 12-2.py
+++ Bài 12-2.py
@@ -7,10 +7,10 @@ Created on Fri Nov 27 10:29:13 2020
 
 import string,random
 R=dict()
-n=random.randrange(1,10)
+n=random.randrange(1,15)
 T=random.choice(string.ascii_uppercase)
 H=T+''.join(random.choice(string.ascii_lowercase) for i in range(n-1))
 R['name']=H
 A=random.randrange(1,100)
 R['age']=A
-print(R)
+print("Dictionary: ",R)
