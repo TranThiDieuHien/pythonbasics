2f9488c1bce3cf6bff25ffc05352f6d936d3fc22
TranThiDieuHien <72079838+TranThiDieuHien@users.noreply.github.com>
2020-12-10 11:06:31 +0700

    Add files via upload

---
+++ baitap 13-3.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  9 23:02:22 2020
+
+@author: DUC-PC
+"""
+
+import os,random
+import string
+t=input('Nhập tên thư mục: ') #Em muốn tạo thư mục trực tiếp trên python
+path='C:\\Users\\DUC-PC\\' 
+os.chdir(path)
+os.mkdir(t)
+i=1
+file_name = input("Nhập tên file dữ liệu: ") #Em muốn tạo tên file trực tiếp trên python
+n=int(input("Nhập tổng số lượng file với dung lượng 1MB-1024MB: ")) #vì 1MB=1024KB nên số lượng file nằm từ khoảng 2->1048 files
+i=1
+for i in range(n):
+    path1 = path + t
+    os.chdir(path1)
+    i=str(i)
+    f=open(file_name + i + '.txt','w+')
+    f.seek(1024*1000-1)
+    f.write(random.choice(string.ascii_lowercase))
+if f.seek(0):
+    os.remove(f) #Lọc file 0KB
+        
