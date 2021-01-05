af9eacbcae933e0aecdf04d6cd483b0878accd94
TranThiDieuHien <72079838+TranThiDieuHien@users.noreply.github.com>
2021-01-05 14:32:34 +0700

    Update Bài 5.py

--- Bài 5.py
+++ Bài 5.py
@@ -6,7 +6,7 @@ Created on Wed Nov 18 20:25:23 2020
 """
 import math
 #Tạo một List gồm cấc số thực
-x=[5,2,3,6]
+x=[5.0,2.0,3.0,6.0]
 #Thực hiện lặp truy xuất đến từng phần tử trong List và in giá trị của từng phần tử ra màn hình
 for pt in x:
     print("Giá trị từng phần tử trong list ",pt)
