24e1c49037ac3205946dffd2208bdadcccee60cc
TranThiDieuHien <72079838+TranThiDieuHien@users.noreply.github.com>
2021-03-07 00:23:36 +0700

    Add files via upload

---
+++ tao_mau_ngau_nhien.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  6 23:34:39 2021
+
+@author: Tran Thi Dieu Hien
+"""
+
+import pandas as pd
+import csv
+import random
+
+file = pd.read_csv('C:\\Users\\DUC-PC\\Downloads\\DSSV.csv')
+print(file.head(10))
+print("-"*50)
+#Chọn ngẫu nhiên 7 sv trong ds
+files = file.sample(n=7)
+files.to_csv(r'C:\\Users\\DUC-PC\\Downloads\\DSSVmoi.csv', index = None, header=True)
+print(files)
