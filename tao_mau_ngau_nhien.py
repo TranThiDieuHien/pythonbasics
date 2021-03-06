12218bfa70d9df65f57333cc82d2bd272fe5103e
TranThiDieuHien <72079838+TranThiDieuHien@users.noreply.github.com>
2021-03-07 00:24:34 +0700

    Update tao_mau_ngau_nhien.py

--- tao_mau_ngau_nhien.py
+++ tao_mau_ngau_nhien.py
@@ -14,5 +14,6 @@ print(file.head(10))
 print("-"*50)
 #Chọn ngẫu nhiên 7 sv trong ds
 files = file.sample(n=7)
+#Lưu vào file csv mới tên DSSVmoi.csv
 files.to_csv(r'C:\\Users\\DUC-PC\\Downloads\\DSSVmoi.csv', index = None, header=True)
 print(files)
